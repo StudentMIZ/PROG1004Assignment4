5d141975eeadfce2faad6ae3f2e52fb9b46c6b79
MZI <imtiazmu@sheridancollege.ca>
2022-03-28 19:29:41 -0400

    Experimenting

--- MuhammadZurayn_Imtiaz_a3.py
+++ MuhammadZurayn_Imtiaz_a3.py
@@ -92,7 +92,21 @@ class Building:
         self._buildName = newbuildName
 
     def createSensors(self, noOfSensors, listOfSensors):
-        pass
+        while True:                                                         # makes it so the function loops or doesn't as long as certain conditions are met
+            try:
+                noOfSensors = int(input("Enter the number of sensors deployed across Sheridan Campus: "))         # asks the user to input an integer value, of how many sensors are deployed in the area
+                if(noOfSensors < 1):                                                                              # this is to ensure the user can't input anything lower than a value of 1
+                    print("Invalid Entry")                                                                          # works to print out a statement letting the user know that they have made an incorrect input
+                    continue                                                                                        # this makes it so that it loops back to the earlier line of code
+            except ValueError:                                                                                      # this is for instances where the user doesn't input the value as an integer, as that is not an accepted entry
+                print("Invalid Entry")
+                continue
+            else:
+                break                                                       # stops the loop as an accepted value was made by the user
+        print()                                                             # prints an empty line of code, giving the output a better look for the user (refer to this explanation when used again later)
+        listOfSensors = countdown(noOfSensors)                              # creates a new variable that is a list of integers made when the derived value of 'sensors_input' is run through the 'countdown' function
+        return listOfSensors 
+        
 
     def printSenInfo(self, buildName):
         pass
@@ -146,6 +160,7 @@ class IotSensors:
     
 
 class Co2Sensors:
+    carbon_values = []
     def __init__(self, co2Levels):
         self._co2Levels = co2Levels
 
@@ -155,14 +170,32 @@ class Co2Sensors:
     def setco2Levels(self, newco2Levels):
         self._co2Levels = newco2Levels
 
-        def sensorPos(self):
-            pass
+    def sensorPos(self, posX, posY):
+        posX = randint(0,100)
+        posY = randint (0,100)
 
-    def sensorReadings(self):
-        pass
+    def sensorReadings(self, co2Levels, carbon_values):
+        for _ in 
+        print("Enter the CO2 Reading (PPM) for Day", _, ":")                      # lets the user know which day's CO2 value they should input
+        while True:
+            try:
+                co2Levels = int(input())                         # asks the user to input a variable under the int class, which will be put into the 'carbon_values' list
+                if(co2Levels < 0):                               # checks if the value of the variable is above 50, or below 20, as that is the given range in the problem
+                    print("Invalid Entry")                          # lets the user know that the input isn't accepted, and they must try again
+                    continue                                        # loops back to the if statement
+            except ValueError:                                      # doesn't accept any other type of value other than of the int class
+                print("Invalid Entry")                              # informs the user that the value is not accepted and to try again
+                continue                                            # loops back to ask the user to input a value again
+            else:
+                break                                               # ends the loop as an accepted value is given
+        carbon_values.append(co2Levels)
 
-    def computeAvg(self):
-        pass
+
+    def computeAvg(self, carbon_values):
+        c_values = round(statistics.mean(carbon_values), 2)         # averages out all the values given by the user, while also rounding the values to 2 decimal points
+        print("Rounded Average Readings", c_values, "PPM")              # prints out the value derived from 'c_values' in the output of the code, for the user to see
+        print()
+        carbon_values.clear()
 
 
 
